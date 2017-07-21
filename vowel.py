402f48ab6392d6888beca2811a04fa94dec22458
VENUJAN <VENUJAN@users.noreply.github.com>
2017-07-21 16:41:58 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,10 @@
+original=raw-input(' eneter the word: ')
+word = original.lower()
+first = word[0]
+if len( original) > 0 original. is alphabat():
+if first == "a" or "e" or "i" or "u":
+print = ("vowel") 
+else 
+print=("constant')
+else
+print = ("empty")
